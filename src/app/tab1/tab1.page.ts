c19606d65b8616fea834c0871dfd5f4974b799f6
VojtaMusilu <v_musilu@utb.cz>
2021-10-31 10:21:46 +0100

    add loading dialog

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,25 +1,34 @@
 import { Component } from '@angular/core';
 import { SearchBooksService } from '../api/search-books.service';
-
+import { LoadingController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  inputBook: String
+  inputBook: String = ''
   bookOutput: String = ''
-  constructor(private searchBooksService: SearchBooksService) {
+  loadingDialog: any
+  constructor(private searchBooksService: SearchBooksService, public loadingController: LoadingController) {
   }
 
   public btnSearchClicked(): void {
-    this.searchBooksService.getBooks(this.inputBook).subscribe((data)=> {
+    this.presentLoading();
+    this.searchBooksService.getBooks(this.inputBook).subscribe((data) => {
       console.log(data);
       this.bookOutput = data['numFound'];
+      this.loadingDialog.dismiss();
     })
   }
 
-  
+  async presentLoading() {
+    this.loadingDialog = await this.loadingController.create(
+      {
+        message: 'Retrieving books ...',
+      });
+    await this.loadingDialog.present();
+  }
 
 }
 
