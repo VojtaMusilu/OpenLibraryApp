8fe35baf76141bda868f2dde73f2b0feef1be60d
VojtaMusilu <88598423+VojtaMusilu@users.noreply.github.com>
2021-11-04 15:06:11 +0100

    better history

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@capacitor/storage';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  KEY_FIRST_NAME = "first_name";
+  firstName = "";
 
+  constructor() { 
+    this.getName();
+  }
+
+
+  private async getName(){
+    const {value} = await Storage.get({
+      key: this.KEY_FIRST_NAME
+    });
+
+    this.firstName = JSON.parse(value);
+  }
+
+  async onInput(event: any) {
+    //console.log(event.target.value);
+
+    await Storage.set({
+      key: this.KEY_FIRST_NAME,
+      value: JSON.stringify(event.target.value),
+    });
+
+  }
 }
