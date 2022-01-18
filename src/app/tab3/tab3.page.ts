75400a06da321f653fb8bd67124353b0d5d36193
VojtaMusilu <v_musilu@utb.cz>
2022-01-18 21:06:23 +0100

    add remove button to library

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Storage } from '@capacitor/storage';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -8,17 +9,15 @@ import { Storage } from '@capacitor/storage';
 })
 export class Tab3Page {
 
-  KEY_FIRST_NAME = "first_name";
   KEY_LIBRARY = "my_library";
-  firstName = "";
   myLibrary: Array<any> = [];
 
-  constructor() { 
-    this.getName();
+  constructor(public alertController: AlertController) { 
+    this.loadLibrary();
   }
 
 
-  private async getName(){
+  private async loadLibrary(){
     const {value} = await Storage.get({
       key: this.KEY_LIBRARY
     });
@@ -27,37 +26,53 @@ export class Tab3Page {
     console.log(this.myLibrary);
   }
 
-  async onInput(event: any) {
-    //console.log(event.target.value);
-
-    await Storage.set({
-      key: this.KEY_FIRST_NAME,
-      value: JSON.stringify(event.target.value),
-    });
-
-  }
-
   async onClick(bookKey:string){
 
-    console.log("bookKey: " + bookKey)
-    var index = this.myLibrary.findIndex(function(item){
-      return item.key === bookKey
+    const alert = await this.alertController.create({
+      header: 'Confirm',
+      message: 'Are you sure you want to delete this book from your library?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            return;
+          },
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+
+            console.log("bookKey: " + bookKey)
+            var index = this.myLibrary.findIndex(function(item){
+              return item.key === bookKey
+            });
+        
+            console.log(index)
+        
+            console.log(this.myLibrary)
+            //delete this.myLibrary[index];
+            this.myLibrary.splice(index,1);
+            console.log(this.myLibrary)
+        
+        
+            
+            Storage.set({
+              key: this.KEY_LIBRARY,
+              value: JSON.stringify(this.myLibrary)
+              
+            })
+          },
+        },
+      ],
     });
 
-    console.log(index)
-
-    console.log(this.myLibrary)
-    //delete this.myLibrary[index];
-    this.myLibrary.splice(index,1);
-    console.log(this.myLibrary)
+    await alert.present();
 
 
-    
-    await Storage.set({
-      key: this.KEY_LIBRARY,
-      value: JSON.stringify(this.myLibrary)
-      
-    })
+
+
     
   }
 }
